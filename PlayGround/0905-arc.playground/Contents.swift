e599225964dce7db7dcd452f36ec23e01721152c
jiun <vonreal20@gmail.com>
2022-09-05 10:37:05 +0900

    🌱 [0905 - 수업] : 순환참조 - weak 실습

--- PlayGround/0905-arc.playground/Contents.swift
+++ PlayGround/0905-arc.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class Guild {
     var name: String
-    var owner: User? // 이 길드장은 누구?
+    weak var owner: User? // 이 길드장은 누구?
     
     init(name: String) {
         self.name = name
@@ -36,11 +36,21 @@ var guild: Guild? = Guild(name: "딸기잼") // Guild: RC 1 인스턴스 생성
 user?.guild = guild // Guild: RC 2
 guild?.owner = user // User: RC 2
 
-// 아래 2줄은 1줄만 입력해도 Deinit이 출력됨, 대신 어떤 코드를 활성화하냐에 따라 출력 순서가 달라짐
-// Deinit이 되는 이유는 guild 내에 생성된 User인스턴스를 해제하면서 User가 완전히 제거 되고 Guild의 owner(User 인스턴스)가 가르킬 곳이 없기 때문에 제거된다.
-guild?.owner = nil // Gulid: RC 1
-// user?.guild = nil // User: RC 1
+user = nil
+//guild = nil
 
-guild = nil // Guild: RC 0 인스턴스 해제
-user = nil // User: RC 0
-// ViewDidLoad
+guild?.owner
+//user?.guild
+
+// << 순환참조 발생 >>
+//user?.guild = guild // Guild: RC 2
+//guild?.owner = user // User: RC 2
+
+//// 아래 2줄은 1줄만 입력해도 Deinit이 출력됨, 대신 어떤 코드를 활성화하냐에 따라 출력 순서가 달라짐
+//// Deinit이 되는 이유는 guild 내에 생성된 User인스턴스를 해제하면서 User가 완전히 제거 되고 Guild의 owner(User 인스턴스)가 가르킬 곳이 없기 때문에 제거된다.
+//guild?.owner = nil // Gulid: RC 1
+//// user?.guild = nil // User: RC 1
+//
+//guild = nil // Guild: RC 0 인스턴스 해제
+//user = nil // User: RC 0
+//// ViewDidLoad

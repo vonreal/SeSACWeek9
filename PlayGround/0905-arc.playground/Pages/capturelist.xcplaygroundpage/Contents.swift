1811d2f1d25be478eeb3915a68dc0491b2f8eea4
jiun <vonreal20@gmail.com>
2022-09-05 11:48:40 +0900

    🌱 [0905 - 수업] : arc - 캡쳐리스트

--- PlayGround/0905-arc.playground/Pages/capturelist.xcplaygroundpage/Contents.swift
+++ PlayGround/0905-arc.playground/Pages/capturelist.xcplaygroundpage/Contents.swift
@@ -24,4 +24,22 @@ user?.introduce
 
 user = nil
 
+func myClosure() {
+    var number = 0
+    print("1: \(number)")
+    
+    let closure: () -> Void = { [number] in // 값을 캡쳐! -> 독립적으로 사용 가능 (해당 인자를 작성하지 않으면 40번 줄에서 값이 변화됨)
+        print("closure: \(number)")
+    }
+    
+    closure()
+    
+    number = 100
+    print("2: \(number)")
+    
+    closure()
+}
+
+myClosure()
+
 //: [Next](@next)
